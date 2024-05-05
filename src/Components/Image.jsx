26d5b77dcf5fd341c6c8c881342c02b71f2e1df2
Nadeem4Code <nadeem4code@gmail.com>
2024-05-05 10:26:33 +0530

    all

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-import { Card, CardContent} from "@mui/material";
+import { Card, CardContent } from "@mui/material";
 // Css
 import "../assets/Css/about.css";
-import girl from '../assets/img/girl.svg'
+import girl from "../assets/img/girl.svg";
 
 const Image = () => {
   return (
     <div style={{ marginTop: "40px" }}>
-  
-    
-          <img
-            className="education"
-            src={girl}
-            alt="education"
-           
-          />
-
-      
+      <img className="education" src={girl} alt="education" />
     </div>
   );
 };
