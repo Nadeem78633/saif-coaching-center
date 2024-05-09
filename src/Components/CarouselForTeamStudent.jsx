fbdb84a6564dfe05f53eaa4fd00a1ddba82fffa5
Nadeem4Code <nadeem4code@gmail.com>
2024-05-09 11:23:00 +0530

    all

--- src/Components/CarouselForTeamStudent.jsx
+++ src/Components/CarouselForTeamStudent.jsx
@@ -16,41 +16,42 @@ const Card = ({ index }) => (
 const CarouselForTeamStudent = () => {
   const isSmallScreen = useMediaQuery("(max-width:600px)");
   return (
-    <div className="container">
-      <h3>React Carousel</h3>
-
-      {isSmallScreen ? (
-        <ReactCaroussel
-          infinite={true}
-          autoplay={true}
-          speed={3} // 0.5s
-          display={{
-            dots: true,
-            arrows: false,
-          }}
-        >
-          <Card />
-          <Card />
-          <Card />
-        </ReactCaroussel>
-      ) : (
-        <ReactCaroussel
-          slidesToShow={3}
-          slidesToScroll={1}
-          infinite={true}
-          style={{ color: "blue" }}
-          display={{
-            dots: true,
-            arrows: true,
-          }}
-        >
-          {Array(9)
-            .fill(0)
-            .map((_, index) => (
-              <Card key={index} index={index} />
-            ))}
-        </ReactCaroussel>
-      )}
+    <div className="padding-20">
+      <div className="carousel-container">
+        <h3>React Carousel</h3>
+        {isSmallScreen ? (
+          <ReactCaroussel
+            infinite={true}
+            autoplay={true}
+            speed={3} // 0.5s
+            display={{
+              dots: true,
+              arrows: false,
+            }}
+          >
+            <Card />
+            <Card />
+            <Card />
+          </ReactCaroussel>
+        ) : (
+          <ReactCaroussel
+            slidesToShow={3}
+            slidesToScroll={1}
+            infinite={true}
+            style={{ color: "blue" }}
+            display={{
+              dots: true,
+              arrows: true,
+            }}
+          >
+            {Array(9)
+              .fill(0)
+              .map((_, index) => (
+                <Card key={index} index={index} />
+              ))}
+          </ReactCaroussel>
+        )}
+      </div>
     </div>
   );
 };
