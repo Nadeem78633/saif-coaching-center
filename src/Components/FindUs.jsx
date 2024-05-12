5396609760d9d783fc161efcf8dcab09f88c448f
Nadeem4Code <nadeem4code@gmail.com>
2024-05-12 10:53:50 +0530

    all

--- src/Components/FindUs.jsx
+++ src/Components/FindUs.jsx
@@ -1,14 +1,16 @@
-import { useMediaQuery, Typography, Avatar } from "@mui/material";
+import { Typography, Avatar } from "@mui/material";
 import React from "react";
 import GroupIcon from "@mui/icons-material/Group";
-
+import facebook from "../assets/img/findUs/facebook.svg";
+import whatsApp from "../assets/img/findUs/whatsapp.svg";
+import location from "../assets/img/findUs/location.svg";
 import "../assets/Css/findUs.css";
 
 const FindUs = () => {
   const icons = [
-    { id: 1, icon: "WhatsApp", title: "WhatsApp" },
-    { id: 2, icon: "Location", title: "Location" },
-    { id: 3, icon: "Facebook", title: "Facebook" },
+    { id: 1, icon: whatsApp, title: "WhatsApp" },
+    { id: 2, icon: location, title: "Location" },
+    { id: 3, icon: facebook, title: "Facebook" },
   ];
 
   return (
@@ -17,15 +19,22 @@ const FindUs = () => {
     >
       <Typography variant="h6" className="find-us">
         <Avatar className="find-us-icon">
-          <GroupIcon sytle={{ color: "white" }} />
+          <GroupIcon style={{ color: "white" }} />
         </Avatar>
         <span>Find Us</span>
       </Typography>
       <div className="find-us-icons-container">
         {icons.map((icon) => (
-          <Avatar>
-            <img src="asd" alt="icons" />
-          </Avatar>
+          <div key={icon.id} className="icon-container">
+            <Avatar className="find-us-avatar">
+              <img
+                src={icon.icon}
+                alt="icons"
+                style={{ display: "block", margin: "auto" }}
+              />
+            </Avatar>
+            <Typography className="icons-title">{icon.title}</Typography>
+          </div>
         ))}
       </div>
     </div>
